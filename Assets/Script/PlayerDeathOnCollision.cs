0dc65a1b9c1943f01b8f0ec06d4f3faec832cdfe
Vanisa Rahma Yunita <vanisarahmayunita06@gmail.com>
2024-10-17 23:38:45 +0700

    Merapikan

--- Assets/Script/PlayerDeathOnCollision.cs
+++ Assets/Script/PlayerDeathOnCollision.cs
@@ -2,32 +2,26 @@ using UnityEngine;
 
 public class PlayerDeathOnCollision : MonoBehaviour
 {
-    public GameObject gameOverPanel;  // Panel yang muncul saat player kalah
-
+    public GameObject gameOverPanel;
     private void Start()
     {
-        // Sembunyikan panel kalah pada awalnya
+
         gameOverPanel.SetActive(false); 
     }
 
     private void OnControllerColliderHit(ControllerColliderHit hit)
     {
-        // Cek apakah player bertabrakan dengan musuh (tag: Enemy)
         if (hit.gameObject.CompareTag("Enemy"))
         {
-            // Hancurkan musuh setelah bertabrakan
             Destroy(hit.gameObject);
-
-            // Langsung panggil kematian
             Die();
         }
     }
 
-    // Fungsi ketika player mati (misalnya, terkena enemy)
     void Die()
     {
         Debug.Log("Player mati. Game over.");
-        gameOverPanel.SetActive(true); // Tampilkan panel kalah
-        Time.timeScale = 0f; // Hentikan permainan
+        gameOverPanel.SetActive(true);
+        Time.timeScale = 0f;
     }
 }
